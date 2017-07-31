bc46536a6ab63cb6b4fc1ad71e2b3dfb06e06ad7
Komal Waman <komalshelar90@gmail.com>
2017-07-31 10:32:22 -0700

    changed routes and handling of record not found exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+  def record_not_found
+    render 'record_not_found'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
 
   get 'product/new'
 
-  post 'product/create'
+  post 'product/new', to: 'product#create', as: 'product_create'
 
   get '/product/:id/edit', to: 'product#edit', as: 'product_edit'
 
-  post '/product/:id/update', to: 'product#update', as: 'product_update'
+  post '/product/:id/edit', to: 'product#update', as: 'product_update'
 
   root 'product#index'
+
+  get "*path" => redirect("/")
+  post "*path" => redirect("/")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
