383bb8be2c2eebaf3f47405059064f7e61e1c4d4
Komal Waman <komalshelar90@gmail.com>
2017-07-31 10:15:22 -0700

    Added Edit functionality for product

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -20,9 +20,17 @@ class ProductController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+ 
+    if @product.update(product_params)
+      redirect_to product_index_path
+    else
+      render 'edit'
+    end
   end
 
   private
